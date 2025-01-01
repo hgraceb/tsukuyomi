6400bf070bbf8f989fca442698afeea12a89ca1f
Flop <kukuzuo@gmail.com>
2025-01-01 22:36:38 +0800

    source: proto 01

--- packages/tsukuyomi_sources/lib/tsukuyomi_sources.dart
+++ packages/tsukuyomi_sources/lib/tsukuyomi_sources.dart
@@ -6,6 +6,7 @@ export 'src/core/source_chapter.dart';
 export 'src/core/source_image.dart';
 export 'src/core/source_manga.dart';
 export 'src/core/source_page.dart';
+export 'src/core/source_proto.dart';
 export 'src/core/source_query.dart';
 export 'src/http/source_http_dio.dart';
 export 'src/http/source_http_dio_interceptor.dart';
