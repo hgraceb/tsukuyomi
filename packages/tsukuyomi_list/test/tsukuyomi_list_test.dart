39ed28c22bca836c682b817b73a65e0152b0a3c4
Flop <kukuzuo@gmail.com>
2025-03-29 14:37:11 +0800

    list: test 07

--- packages/tsukuyomi_list/test/tsukuyomi_list_test.dart
+++ packages/tsukuyomi_list/test/tsukuyomi_list_test.dart
@@ -4,7 +4,8 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tsukuyomi_list/src/tsukuyomi_list.dart';
 
-void expectList({List<int> visible = const [], List<int> hidden = const []}) {
+void expectList({int length = 0, List<int> visible = const []}) {
+  final hidden = length == 0 ? const <int>{} : List.generate(length, (i) => i).toSet().difference(visible.toSet());
   for (final i in visible) {
     expect(find.text('$i'), findsOneWidget);
   }
@@ -29,15 +30,15 @@ void main() {
 
     // 从第一个元素开始显示
     await tester.pumpWidget(builder(0));
-    expectList(visible: [0, 1], hidden: [2]);
+    expectList(visible: [0, 1]);
 
     // 从第二个元素开始显示
     await tester.pumpWidget(builder(1));
-    expectList(visible: [1, 2], hidden: [0]);
+    expectList(visible: [1, 2]);
 
     // 从第三个元素开始显示
     await tester.pumpWidget(builder(2));
-    expectList(visible: [2], hidden: [0, 1]);
+    expectList(visible: [2]);
   });
 
   testWidgets('TsukuyomiList respects TsukuyomiListController.jumpToIndex', (WidgetTester tester) async {
@@ -56,22 +57,22 @@ void main() {
 
     // 默认只显示前面两个元素
     await tester.pumpWidget(builder(0));
-    expectList(visible: [0, 1], hidden: [2]);
+    expectList(visible: [0, 1]);
 
     // 可以跳转到当前位置的元素
     controller.jumpToIndex(0);
     await tester.pump();
-    expectList(visible: [0, 1], hidden: [2]);
+    expectList(visible: [0, 1]);
 
     // 可以跳转到指定位置的元素
     controller.jumpToIndex(1);
     await tester.pump();
-    expectList(visible: [1, 2], hidden: [0]);
+    expectList(visible: [1, 2]);
 
     // 可以跳转到指定位置的元素并越界显示
     controller.jumpToIndex(2);
     await tester.pump();
-    expectList(visible: [2], hidden: [0, 1]);
+    expectList(visible: [2]);
   });
 
   testWidgets('TsukuyomiList respects TsukuyomiListController.slideViewport', (WidgetTester tester) async {
@@ -90,20 +91,20 @@ void main() {
 
     // 默认只显示前面两个元素
     await tester.pumpWidget(builder(0));
-    expectList(visible: [0, 1], hidden: [2]);
+    expectList(visible: [0, 1]);
 
     // 滚动半个屏幕的距离
     unawaited(controller.slideViewport(0.5));
     await tester.pump();
-    expectList(visible: [0, 1], hidden: [2]);
+    expectList(visible: [0, 1]);
     await tester.pumpAndSettle();
-    expectList(visible: [1, 2], hidden: [0]);
+    expectList(visible: [1, 2]);
 
     // 不进行越界滚动
     unawaited(controller.slideViewport(0.5));
     await tester.pump();
-    expectList(visible: [1, 2], hidden: [0]);
+    expectList(visible: [1, 2]);
     await tester.pumpAndSettle();
-    expectList(visible: [1, 2], hidden: [0]);
+    expectList(visible: [1, 2]);
   });
 }
