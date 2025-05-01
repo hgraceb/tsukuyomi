50d3953c1f129ab701a13d5f2f2928f8a7fca246
Flop <kukuzuo@gmail.com>
2025-05-01 21:00:43 +0800

    list: dynamic items 72

--- lib/pages/debug/list/debug_list_page.dart
+++ lib/pages/debug/list/debug_list_page.dart
@@ -15,17 +15,14 @@ class DebugListPage extends StatefulWidget {
 class _DebugListPageState extends State<DebugListPage> {
   int id = 0;
   final random = Random(2147483647);
-  late final itemKeys = List.generate(2000, (index) => id++);
-  late final itemHeights = List.generate(itemKeys.length, (index) => 100.0 + (997 <= index && index <= 1008 ? 0.0 : random.nextInt(100)));
+  late final itemKeys = List.generate(20, (index) => id++);
+  late final itemHeights = List.generate(itemKeys.length, (index) => 100.0 + random.nextInt(100));
   late final controller = TsukuyomiListController();
 
   @override
   void initState() {
     super.initState();
-    SchedulerBinding.instance.addPostFrameCallback((timeStamp) async {
-      await controller.slideViewport(-1.0);
-      await controller.slideViewport(-0.5);
-    });
+    SchedulerBinding.instance.addPostFrameCallback((timeStamp) => controller.slideViewport(-1.0));
   }
 
   @override
@@ -34,12 +31,9 @@ class _DebugListPageState extends State<DebugListPage> {
       body: GestureDetector(
         onTap: () {
           setState(() {
-            final adding = random.nextInt(100);
-            final removing = random.nextInt(100);
+            final removing = random.nextInt(8);
             itemKeys.removeRange(0, removing);
             itemHeights.removeRange(0, removing);
-            itemKeys.insertAll(adding, List.generate(adding, (index) => id));
-            itemHeights.insertAll(adding, List.generate(adding, (index) => 100.0 + random.nextInt(100)));
           });
         },
         child: Center(
@@ -48,7 +42,7 @@ class _DebugListPageState extends State<DebugListPage> {
             child: TsukuyomiList.builder(
               debugMask: true,
               anchor: 0.5,
-              initialScrollIndex: (itemKeys.length - 1).clamp(0, 1003),
+              initialScrollIndex: (itemKeys.length - 1).clamp(0, 19),
               controller: controller,
               itemKeys: itemKeys,
               itemBuilder: (context, index) => SizedBox(
