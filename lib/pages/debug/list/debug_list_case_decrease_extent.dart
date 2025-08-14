5bd747008eb2389f9248e15f2b451a4a876a38dc
Flop <kukuzuo@gmail.com>
2025-08-14 20:10:36 +0800

    list: dynamic items 44 修改列表测试示例

--- lib/pages/debug/list/debug_list_case_decrease_extent.dart
+++ lib/pages/debug/list/debug_list_case_decrease_extent.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: invalid_use_of_internal_member
+
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:tsukuyomi/core/core.dart';
 import 'package:tsukuyomi_list/tsukuyomi_list.dart';
 
@@ -10,46 +13,57 @@ class DebugListCaseDecreaseExtent extends StatefulWidget {
 }
 
 class _DebugListCaseDecreaseExtentState extends State<DebugListCaseDecreaseExtent> {
-  int step = 0;
-  final itemKeys = List.generate(10, (index) => index);
+  double heightFactor = 2.0;
+  final itemKeys = List.generate(20, (index) => index);
+  final itemHeights = List.generate(20, (index) => 100.0);
   final controller = TsukuyomiListController();
 
-  Widget builder({required List<double> itemHeights}) {
-    return Directionality(
-      textDirection: TextDirection.ltr,
-      child: TsukuyomiList.builder(
-        debugMask: true,
-        itemKeys: itemKeys,
-        itemBuilder: (context, index) => SizedBox(height: itemHeights[index], child: Placeholder(child: Text('${itemKeys[index]}'))),
-        controller: controller,
-        anchor: 0.5,
-      ),
-    );
+  ScrollPosition get position {
+    return controller.position;
+  }
+
+  void onScroll() {
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    SchedulerBinding.instance.addPostFrameCallback((timeStamp) async {
+      position.addListener(onScroll);
+      await Future.delayed(const Duration(seconds: 1));
+      await controller.slideViewport(1.0);
+      heightFactor = 1.0;
+      await controller.slideViewport(-1.0, curve: Curves.linear, duration: const Duration(seconds: 10));
+    });
+  }
+
+  @override
+  void dispose() {
+    position.removeListener(onScroll);
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return TsukuyomiScaffold(
-      body: GestureDetector(
-        onTap: () async {
-          if (step == 0) {
-            await controller.slideViewport(0.5);
-            setState(() => step++);
-          } else {
-            setState(() {
-
-            });
-          }
-        },
-        child: Center(
-          child: SizedBox(
-            height: 600.0,
-            child: builder(
-              itemHeights: switch (step) {
-                0 => List.generate(itemKeys.length, (index) => 100.0),
-                1 => List.generate(itemKeys.length, (index) => 200.0),
-                _ => throw AssertionError(step),
-              },
+      body: Center(
+        child: SizedBox(
+          height: 600.0,
+          child: TsukuyomiList.builder(
+            anchor: 0.5,
+            debugMask: true,
+            trailing: false,
+            controller: controller,
+            itemKeys: itemKeys,
+            itemBuilder: (context, index) => FutureBuilder(
+              future: index == 1 ? Future.delayed(const Duration(milliseconds: 200), () => itemHeights[index] * heightFactor) : null,
+              builder: (context, snapshot) => SizedBox(
+                height: snapshot.data ?? itemHeights[index],
+                child: Placeholder(
+                  child: Text('${itemKeys[index]} [${snapshot.data ?? itemHeights[index]}]'),
+                ),
+              ),
             ),
           ),
         ),
