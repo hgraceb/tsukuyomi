897a1aa0baf3d33b92f35079f05e7db512911a4b
Flop <kukuzuo@gmail.com>
2025-09-02 22:05:57 +0800

    list: dynamic items 78 修复单元测试 anchor when remove multi at edge

--- lib/pages/debug/list/debug_list_page.dart
+++ lib/pages/debug/list/debug_list_page.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-import 'debug_list_case_anchor_remove_edge_single.dart';
+import 'debug_list_case_anchor_remove_edge_multi.dart';
 
 class DebugListPage extends StatelessWidget {
   const DebugListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const DebugListCaseAnchorRemoveEdgeSingle();
+    return const DebugListCaseAnchorRemoveEdgeMulti();
   }
 }
 
