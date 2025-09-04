604e0d21cb6fb62f4435da15834b47da558e6bbc
Flop <kukuzuo@gmail.com>
2025-09-04 21:34:45 +0800

    list: dynamic items 83 新增单元测试

--- lib/pages/debug/list/debug_list_page.dart
+++ lib/pages/debug/list/debug_list_page.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-import 'debug_list_case_set_state_tsukuyomi.dart';
+import 'debug_list_case_slide_default.dart';
 
 class DebugListPage extends StatelessWidget {
   const DebugListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const DebugListCaseSetStateTsukuyomi();
+    return const DebugListCaseSlideDefault();
   }
 }
 
