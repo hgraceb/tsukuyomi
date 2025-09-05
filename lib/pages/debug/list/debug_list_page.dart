490c53b9c6aa68c8bfba07f359fff38609e98ede
Flop <kukuzuo@gmail.com>
2025-09-05 21:24:53 +0800

    list: dynamic items 86 新增锚点移除测试

--- lib/pages/debug/list/debug_list_page.dart
+++ lib/pages/debug/list/debug_list_page.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-import 'debug_list_case_slide_bouncing.dart';
+import 'debug_list_case_anchor_remove_anchor_single.dart';
 
 class DebugListPage extends StatelessWidget {
   const DebugListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const DebugListCaseSlideBouncing();
+    return const DebugListCaseAnchorRemoveAnchorSingle();
   }
 }
 
