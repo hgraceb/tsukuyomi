93c5efb5a7216bff7bf02daa4f87a3406a39d8aa
Flop <kukuzuo@gmail.com>
2025-09-07 20:37:49 +0800

    list: 05 测试动态移除

--- lib/pages/debug/list/debug_list_page.dart
+++ lib/pages/debug/list/debug_list_page.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-import 'debug_list_case_anchor_add_anchor_single.dart';
+import 'debug_list_case_anchor_remove_edge_single.dart';
 
 class DebugListPage extends StatelessWidget {
   const DebugListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const DebugListCaseAnchorAddAnchorSingle();
+    return const DebugListCaseAnchorRemoveEdgeSingle();
   }
 }
 
