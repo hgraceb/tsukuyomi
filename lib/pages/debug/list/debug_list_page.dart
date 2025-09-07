1535d7119d7974e4c390d6bea6b8e29903d3f719
Flop <kukuzuo@gmail.com>
2025-09-07 15:55:06 +0800

    list: 02 测试动态添加列表项

--- lib/pages/debug/list/debug_list_page.dart
+++ lib/pages/debug/list/debug_list_page.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-import 'debug_list_case_anchor_default.dart';
+import 'debug_list_case_anchor_add_anchor_single.dart';
 
 class DebugListPage extends StatelessWidget {
   const DebugListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const DebugListCaseAnchorDefault();
+    return const DebugListCaseAnchorAddAnchorSingle();
   }
 }
 
