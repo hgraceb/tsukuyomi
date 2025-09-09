b7b33779de34aa1620d073e49bc0fe43d48c584c
Flop <kukuzuo@gmail.com>
2025-09-09 21:28:23 +0800

    list: 09 测试列表尺寸缓存

--- lib/pages/debug/list/debug_list_page.dart
+++ lib/pages/debug/list/debug_list_page.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-import 'debug_list_case_slide_jump_remove.dart';
+import 'debug_list_case_anchor_default.dart';
 
 class DebugListPage extends StatelessWidget {
   const DebugListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const DebugListCaseSlideJumpRemove();
+    return const DebugListCaseAnchorDefault();
   }
 }
 
