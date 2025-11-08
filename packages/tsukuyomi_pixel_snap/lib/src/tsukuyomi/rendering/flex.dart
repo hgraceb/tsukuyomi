ad390e79931c57e21c0d1ae77bacfaf3d2c28446
Flop <kukuzuo@gmail.com>
2025-11-08 18:25:35 +0800

    upgrade: tsukuyomi pixel snap

--- packages/tsukuyomi_pixel_snap/lib/src/tsukuyomi/rendering/flex.dart
+++ packages/tsukuyomi_pixel_snap/lib/src/tsukuyomi/rendering/flex.dart
@@ -2,6 +2,7 @@ import 'dart:math' as math;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/rendering.dart';
+import 'package:tsukuyomi_pixel_snap/src/pixel_snap.dart';
 
 part 'package:tsukuyomi_pixel_snap/src/flutter/rendering/flex.dart';
 
@@ -84,7 +85,12 @@ class TsukuyomiPixelRenderFlex extends RenderFlex {
       }
       totalFlex -= flex;
       assert(spacePerFlex.isFinite);
-      final double maxChildExtent = spacePerFlex * flex;
+      // region Tsukuyomi: 修改尺寸计算方法
+      // ```
+      // final double maxChildExtent = spacePerFlex * flex;
+      // ```
+      final double maxChildExtent = (spacePerFlex * flex).pixelSnap();
+      // endregion Tsukuyomi
       assert(_getFit(child) == FlexFit.loose || maxChildExtent < double.infinity);
       final BoxConstraints childConstraints = _constraintsForFlexChild(
         child,
@@ -129,122 +135,4 @@ class TsukuyomiPixelRenderFlex extends RenderFlex {
       spacePerFlex: firstFlexChild == null ? null : spacePerFlex,
     );
   }
-
-  // @override
-  // _LayoutSizes _computeSizes({required BoxConstraints constraints, required ChildLayouter layoutChild}) {
-  //   assert(_debugHasNecessaryDirections);
-  //
-  //   // Determine used flex factor, size inflexible items, calculate free space.
-  //   int totalFlex = 0;
-  //   final double maxMainSize = _direction == Axis.horizontal ? constraints.maxWidth : constraints.maxHeight;
-  //   final bool canFlex = maxMainSize < double.infinity;
-  //
-  //   double crossSize = 0.0;
-  //   double allocatedSize = 0.0; // Sum of the sizes of the non-flexible children.
-  //   RenderBox? child = firstChild;
-  //   RenderBox? lastFlexChild;
-  //   while (child != null) {
-  //     final FlexParentData childParentData = child.parentData! as FlexParentData;
-  //     final int flex = _getFlex(child);
-  //     if (flex > 0) {
-  //       totalFlex += flex;
-  //       lastFlexChild = child;
-  //     } else {
-  //       final BoxConstraints innerConstraints;
-  //       if (crossAxisAlignment == CrossAxisAlignment.stretch) {
-  //         switch (_direction) {
-  //           case Axis.horizontal:
-  //             innerConstraints = BoxConstraints.tightFor(height: constraints.maxHeight);
-  //           case Axis.vertical:
-  //             innerConstraints = BoxConstraints.tightFor(width: constraints.maxWidth);
-  //         }
-  //       } else {
-  //         switch (_direction) {
-  //           case Axis.horizontal:
-  //             innerConstraints = BoxConstraints(maxHeight: constraints.maxHeight);
-  //           case Axis.vertical:
-  //             innerConstraints = BoxConstraints(maxWidth: constraints.maxWidth);
-  //         }
-  //       }
-  //       final Size childSize = layoutChild(child, innerConstraints);
-  //       allocatedSize += _getMainSize(childSize);
-  //       crossSize = math.max(crossSize, _getCrossSize(childSize));
-  //     }
-  //     assert(child.parentData == childParentData);
-  //     child = childParentData.nextSibling;
-  //   }
-  //
-  //   // Distribute free space to flexible children.
-  //   final double freeSpace = math.max(0.0, (canFlex ? maxMainSize : 0.0) - allocatedSize);
-  //   double allocatedFlexSpace = 0.0;
-  //   if (totalFlex > 0) {
-  //     final double spacePerFlex = canFlex ? (freeSpace / totalFlex) : double.nan;
-  //     child = firstChild;
-  //     while (child != null) {
-  //       final int flex = _getFlex(child);
-  //       if (flex > 0) {
-  //         // region Tsukuyomi: 修改尺寸计算方法
-  //         final double maxChildExtent = canFlex ? (child == lastFlexChild ? (freeSpace - allocatedFlexSpace) : spacePerFlex * flex).pixelSnap() : double.infinity;
-  //         // endregion Tsukuyomi
-  //         late final double minChildExtent;
-  //         switch (_getFit(child)) {
-  //           case FlexFit.tight:
-  //             assert(maxChildExtent < double.infinity);
-  //             minChildExtent = maxChildExtent;
-  //           case FlexFit.loose:
-  //             minChildExtent = 0.0;
-  //         }
-  //         final BoxConstraints innerConstraints;
-  //         if (crossAxisAlignment == CrossAxisAlignment.stretch) {
-  //           switch (_direction) {
-  //             case Axis.horizontal:
-  //               innerConstraints = BoxConstraints(
-  //                 minWidth: minChildExtent,
-  //                 maxWidth: maxChildExtent,
-  //                 minHeight: constraints.maxHeight,
-  //                 maxHeight: constraints.maxHeight,
-  //               );
-  //             case Axis.vertical:
-  //               innerConstraints = BoxConstraints(
-  //                 minWidth: constraints.maxWidth,
-  //                 maxWidth: constraints.maxWidth,
-  //                 minHeight: minChildExtent,
-  //                 maxHeight: maxChildExtent,
-  //               );
-  //           }
-  //         } else {
-  //           switch (_direction) {
-  //             case Axis.horizontal:
-  //               innerConstraints = BoxConstraints(
-  //                 minWidth: minChildExtent,
-  //                 maxWidth: maxChildExtent,
-  //                 maxHeight: constraints.maxHeight,
-  //               );
-  //             case Axis.vertical:
-  //               innerConstraints = BoxConstraints(
-  //                 maxWidth: constraints.maxWidth,
-  //                 minHeight: minChildExtent,
-  //                 maxHeight: maxChildExtent,
-  //               );
-  //           }
-  //         }
-  //         final Size childSize = layoutChild(child, innerConstraints);
-  //         final double childMainSize = _getMainSize(childSize);
-  //         assert(childMainSize <= maxChildExtent);
-  //         allocatedSize += childMainSize;
-  //         allocatedFlexSpace += maxChildExtent;
-  //         crossSize = math.max(crossSize, _getCrossSize(childSize));
-  //       }
-  //       final FlexParentData childParentData = child.parentData! as FlexParentData;
-  //       child = childParentData.nextSibling;
-  //     }
-  //   }
-  //
-  //   final double idealSize = canFlex && mainAxisSize == MainAxisSize.max ? maxMainSize : allocatedSize;
-  //   return _LayoutSizes(
-  //     mainSize: idealSize,
-  //     crossSize: crossSize,
-  //     allocatedSize: allocatedSize,
-  //   );
-  // }
 }
