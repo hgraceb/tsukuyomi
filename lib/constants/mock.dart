b1c96f3fe4d3916dfb781911a6140a61bacd3177
Flop <kukuzuo@gmail.com>
2025-11-08 20:56:03 +0800

    upgrade: tsukuyomi main mock

--- lib/constants/mock.dart
+++ lib/constants/mock.dart
@@ -15,11 +15,11 @@ class MockSource {
   MockSource._({required this.name, required this.path, required this.enabled, required this.mangas});
 
   factory MockSource.fromJson(Map<String, dynamic> json) => MockSource._(
-        name: json['name'],
-        path: json['path'],
-        enabled: json['enabled'],
-        mangas: (json['mangas'] as List).map((e) => MockManga.fromJson(e)).toList(),
-      );
+    name: json['name'],
+    path: json['path'],
+    enabled: json['enabled'],
+    mangas: (json['mangas'] as List).map((e) => MockManga.fromJson(e)).toList(),
+  );
 
   final String name;
 
@@ -38,11 +38,11 @@ class MockManga {
   MockManga._({required this.id, required this.url, required this.title, required this.cover});
 
   factory MockManga.fromJson(Map<String, dynamic> json) => MockManga._(
-        id: json['id'],
-        url: json['url'],
-        title: json['title'],
-        cover: json['cover'],
-      );
+    id: json['id'],
+    url: json['url'],
+    title: json['title'],
+    cover: json['cover'],
+  );
 
   final int id;
 
@@ -54,31 +54,36 @@ class MockManga {
 }
 
 Future<MockDatabase> getMockDatabase() async {
-  final decoded = jsonDecode(await rootBundle.loadString('assets/source/index.json')) as List<dynamic>;
-  final sources = decoded.map((e) => MockSource.fromJson(e as Map<String, dynamic>)).where((e) => e.enabled).toList();
-  return MockDatabase._(
-    sources: [
-      for (final source in sources) ...[
-        DatabaseSource(
-          id: source.id,
-          name: source.nameWithPath,
-          source: base64.encode(utf8.encode(await rootBundle.loadString(source.path))),
-        ),
-      ],
-    ],
-    mangas: [
-      for (final source in sources) ...[
-        for (final manga in source.mangas) ...[
-          DatabaseManga(
-            id: manga.id,
-            source: source.id,
-            url: manga.url,
-            title: manga.title,
-            cover: manga.cover,
-            favorite: true,
+  try {
+    final decoded = jsonDecode(await rootBundle.loadString('assets/source/index.json')) as List<dynamic>;
+    final sources = decoded.map((e) => MockSource.fromJson(e as Map<String, dynamic>)).where((e) => e.enabled).toList();
+    return MockDatabase._(
+      sources: [
+        for (final source in sources) ...[
+          DatabaseSource(
+            id: source.id,
+            name: source.nameWithPath,
+            source: base64.encode(utf8.encode(await rootBundle.loadString(source.path))),
           ),
-        ]
-      ]
-    ],
-  );
+        ],
+      ],
+      mangas: [
+        for (final source in sources) ...[
+          for (final manga in source.mangas) ...[
+            DatabaseManga(
+              id: manga.id,
+              source: source.id,
+              url: manga.url,
+              title: manga.title,
+              cover: manga.cover,
+              favorite: true,
+            ),
+          ],
+        ],
+      ],
+    );
+  } catch (e) {
+    // TODO 判断是否需要通过控制台日志提示可以进行本地模拟数据配置
+    return MockDatabase._(sources: [], mangas: []);
+  }
 }
