f9be49b2bf747f5173d6fbac55489ec90461e5f7
Flop <kukuzuo@gmail.com>
2025-11-08 16:39:42 +0800

    upgrade: tsukuyomi metadata

--- packages/tsukuyomi_metadata/android/src/main/kotlin/com/flop/tsukuyomi_metadata/TsukuyomiMetadataPlugin.kt
+++ packages/tsukuyomi_metadata/android/src/main/kotlin/com/flop/tsukuyomi_metadata/TsukuyomiMetadataPlugin.kt
@@ -8,22 +8,24 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** TsukuyomiMetadataPlugin */
-class TsukuyomiMetadataPlugin : FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel: MethodChannel
+class TsukuyomiMetadataPlugin :
+    FlutterPlugin,
+    MethodCallHandler {
+    // The MethodChannel that will the communication between Flutter and native Android
+    //
+    // This local reference serves to register the plugin with the Flutter Engine and unregister it
+    // when the Flutter Engine is detached from the Activity
+    private lateinit var channel: MethodChannel
 
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "tsukuyomi_metadata")
-    channel.setMethodCallHandler(this)
-  }
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "tsukuyomi_metadata")
+        channel.setMethodCallHandler(this)
+    }
 
   override fun onMethodCall(call: MethodCall, result: Result) {
     when (call.method) {
       "getPlatformVersion" -> {
-        result.success("Android ${android.os.Build.VERSION.RELEASE}")
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
       }
 
       "getStatusBarHeight" -> {
@@ -33,12 +35,12 @@ class TsukuyomiMetadataPlugin : FlutterPlugin, MethodCallHandler {
       }
 
       else -> {
-        result.notImplemented()
-      }
+            result.notImplemented()
+        }
     }
   }
 
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 }
