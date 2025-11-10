ae9cfc19bd820a3a1d7f8938c47fff2b609239dc
Flop <kukuzuo@gmail.com>
2025-11-10 22:02:44 +0800

    upgrade: tsukuyomi pixel snap

---
+++ packages/tsukuyomi_pixel_snap/test/tsukuyomi_widgets_basic_test.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tsukuyomi_pixel_snap/src/tsukuyomi/widgets/basic.dart';
+
+void main() {
+  group('TsukuyomiPixelRow', () {
+    testWidgets('space spread evenly', (WidgetTester tester) async {
+      final itemCount = 99;
+      await tester.pumpWidget(
+        Directionality(
+          textDirection: TextDirection.ltr,
+          child: TsukuyomiPixelRow(children: List.generate(itemCount, (index) => Expanded(child: Text('$index')))),
+        ),
+      );
+      for (var i = 0; i < itemCount; i++) {
+        expect(tester.getSize(find.text('$i')).width, anyOf([equals(24.0 / 3), equals(25.0 / 3)]));
+      }
+    });
+  });
+
+  group('TsukuyomiPixelColumn', () {
+    testWidgets('space spread evenly', (WidgetTester tester) async {
+      final itemCount = 99;
+      await tester.pumpWidget(
+        Directionality(
+          textDirection: TextDirection.ltr,
+          child: TsukuyomiPixelColumn(children: List.generate(itemCount, (index) => Expanded(child: Text('$index')))),
+        ),
+      );
+      for (var i = 0; i < itemCount; i++) {
+        expect(tester.getSize(find.text('$i')).height, anyOf([equals(18.0 / 3), equals(19.0 / 3)]));
+      }
+    });
+  });
+}
