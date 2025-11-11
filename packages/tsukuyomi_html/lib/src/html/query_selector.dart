eac7af1af665cbd86d64475adaf4b35fd5164e51
Flop <kukuzuo@gmail.com>
2025-11-11 21:08:39 +0800

    upgrade: tsukuyomi html

--- packages/tsukuyomi_html/lib/src/html/query_selector.dart
+++ packages/tsukuyomi_html/lib/src/html/query_selector.dart
@@ -1,12 +1,10 @@
-// ignore_for_file: avoid_relative_lib_imports
 library;
 
-import '../../dom.dart';
-import '../../html/lib/src/constants.dart' show isWhitespaceCC;
-import '../csslib/parser.dart';
-import '../../csslib/lib/visitor.dart';
-
-part '../../html/lib/src/query_selector.dart';
+import 'package:tsukuyomi_html/csslib/lib/visitor.dart';
+import 'package:tsukuyomi_html/dom.dart';
+import 'package:tsukuyomi_html/html/lib/src/constants.dart' show isWhitespaceCC;
+import 'package:tsukuyomi_html/src/csslib/parser.dart';
+part 'package:tsukuyomi_html/html/lib/src/query_selector.dart';
 
 /// [_parseSelectorList]
 SelectorGroup tsukuyomiParseSelectorList(String selector) {
