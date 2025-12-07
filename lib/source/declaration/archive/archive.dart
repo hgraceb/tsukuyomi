4ddd7427b80d7f9650322d89a448fdd6b060edd4
Flop <kukuzuo@gmail.com>
2025-12-07 21:50:04 +0800

    source: 03 新增解压缩方法定义

--- lib/source/declaration/archive/archive.dart
+++ lib/source/declaration/archive/archive.dart
@@ -1,12 +1,14 @@
 import 'package:tsukuyomi_eval/tsukuyomi_eval.dart';
 
 import 'src/archive.dart';
+import 'src/archive_file.dart';
 import 'src/zip_decoder.dart';
 
 List<DartLibrary> get $archiveLibraries {
   return [
     DartLibrary('archive', path: 'archive.dart', source: 'library archive;', declarations: [
       ...$archive,
+      ...$archiveFile,
       ...$zipDecoder,
     ]),
   ];
