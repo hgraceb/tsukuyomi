ec5139ead498a138e47c7a83d6879171fceda918
Flop <kukuzuo@gmail.com>
2025-12-15 21:42:43 +0800

    source: 01 修复文件路径错误

--- lib/extension/extension_string.dart
+++ lib/extension/extension_string.dart
@@ -1,8 +1,11 @@
 extension StringExtension on String {
-  /// 去除首尾空白字符并将部分半角字符替换为全角字符（“\”、“/”、“:”、“*”、“?”、“"”、“<”、“>”、“|”）
-  /// TODO 判断是否添加更多的半角字符规则，如：将“!”转换为“！”以保持文件夹命名统一，特别要注意半角空格的转换不能简单加 65248
-  String get toLegalPath {
-    assert(trim().isNotEmpty);
-    return trim().replaceAllMapped(RegExp(r'[\\/:*?"<>|]'), (match) => String.fromCharCode(match[0]!.codeUnits[0] + 0xfee0));
-  }
+  /// 将 Windows 非法字符（"\"、"/"、":"、"*"、"?"、"""、"<"、">"、"|"）替换为对应的全角字符
+  /// TODO 判断是否添加更多的半角字符规则，如：将"!"转换为"！"以保持文件夹命名统一，特别要注意半角空格的转换不能简单加 65248
+  String get toFullWidthIllegalChars => replaceAllMapped(RegExp(r'[\\/:*?"<>|]'), (m) => String.fromCharCode(m[0]!.codeUnits[0] + 0xfee0));
+
+  /// 将末尾单独或连续的 "." 全部替换为 "․" (One Dot Leader U+2024) 并去除空格
+  String get toSafeTrailing => replaceAllMapped(RegExp(r'[. ]+$'), (m) => m[0]!.replaceAll('.', '\u2024').replaceAll(' ', ''));
+
+  /// 去除首尾空白字符并转换为合法的文件路径名
+  String get toLegalPath => trim().toFullWidthIllegalChars.toSafeTrailing;
 }
